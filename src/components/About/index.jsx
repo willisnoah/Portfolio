c89bd80bdc12d96f9871b57f58cf6abc0570b32f
Noah Willis <noahwillis@Noahs-MacBook-Air.local>
2023-10-08 14:20:37 -0700

    Transferred code into repo

---
+++ src/components/About/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        </p>
+        <p>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
