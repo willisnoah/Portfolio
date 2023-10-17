74bb2f079ee96decbf2c8c3652ae78c7a19a0c71
Noah Willis <noahwillis@Noahs-MacBook-Air.local>
2023-10-17 09:03:09 -0700

    Add projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,34 +6,28 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'surf-report',
-      description: 'MERN Stack',
+      name: 'password-generator',
+      description: 'HTML/CSS/JS',
+      link: "https://github.com/willisnoah/Random-Password_Gen",
+      repo: "https://github.com/willisnoah/Random-Password_Gen"
+    },
+    {
+      name: 'work-scheduler',
+      description: 'HTML/CSS/JS',
+      link: "https://github.com/willisnoah/Work-Scheduler",
+      repo: "https://github.com/willisnoah/Work-Scheduler"
+    },
+    {
+      name: 'bueche-business-coaching',
+      description: 'REACT',
       link: "https://github.com",
       repo: "https://github.com"
     },
     {
-      name: 'pastel-puzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'led-wall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
-    },
-    {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'logo-creator',
+      description: 'JS',
+      link: "https://github.com/willisnoah/Logo-creator",
+      repo: "https://github.com/willisnoah/Logo-creator"
     },
   ]);
 
